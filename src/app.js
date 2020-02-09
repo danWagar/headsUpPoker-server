6c964f6194d42bb9e1495a306ab286afff6a9e5f
danWagar <dandwagar@gmail.com>
2020-02-08 16:28:46 -0800

    initial commit

---
+++ src/app.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const authRouter = require('./auth/auth-router');
+const userRouter = require('./user/user-router');
+
+const app = express();
+
+app.use(
+  morgan(NODE_ENV === 'production' ? 'tiny' : 'common', {
+    skip: () => NODE_ENV === 'test'
+  })
+);
+app.use(cors());
+app.use(helmet());
+
+app.get('/', (req, res) => {
+  res.send('Hello boilerplate');
+});
+
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+
+module.exports = app;
