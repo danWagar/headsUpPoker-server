6706ca586ee89cc608b840c9d78cc5520cac36b4
danWagar <dandwagar@gmail.com>
2020-02-13 12:49:45 -0800

    define data tables for migration script

---
+++ migrations/002.do.create-game-tables.sql
@@ -0,0 +1,54 @@
+
+CREATE TYPE card AS ENUM (
+		"BAD", "2c", "2d", "2h", "2s",
+		"3c", "3d", "3h", "3s",
+		"4c", "4d", "4h", "4s",
+		"5c", "5d", "5h", "5s",
+		"6c", "6d", "6h", "6s",
+		"7c", "7d", "7h", "7s",
+		"8c", "8d", "8h", "8s",
+		"9c", "9d", "9h", "9s",
+		"Tc", "Td", "Th", "Ts",
+		"Jc", "Jd", "Jh", "Js",
+		"Qc", "Qd", "Qh", "Qs",
+		"Kc", "Kd", "Kh", "Ks",
+		"Ac", "Ad", "Ah", "As"
+);
+
+CREATE TYPE bet_type AS ENUM (
+  "check",
+  "call",
+  "raise",
+  "fold"
+)
+
+CREATE TABLE game (
+  id SERIAL PRIMARY KEY,
+  player1_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+  player2_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+  player1_stack INTEGER NOT NULL,
+  player2_stack INTEGER NOT NULL,
+  blind_level INTEGER NOT NULL DEFAULT 0,
+  start_time TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE TABLE hand (
+  id SERIAL PRIMARY KEY,
+  game_id INTEGER REFERENCES game(id) ON DELETE CASCADE NOT NULL,
+  player1_hand1 card NOT NULL,
+  player1_hand2 card NOT NULL,
+  player2_hand1 card NOT NULL,
+  player2_hand2 card NOT NULL,
+  flop_1 card NOT NULL,
+  flop_2 card NOT NULL,
+  flop_3 card NOT NULL,
+  turn card NOT NULL,
+  river card NOT NULL
+)
+
+CREATE TABLE current_bet {
+  id SERIAL PRIMARY KEY,
+  hand_id INTEGER REFERENCES hand(id) ON DELETE CASCADE NOT NULL,
+  player INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+  type bet_type NOT NULL
+}
